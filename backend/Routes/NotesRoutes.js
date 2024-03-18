09641e788419ff4586a08cb3154b9c7f95e422a0
Salma EL HANI <129634703+SalmaELHANI@users.noreply.github.com>
2024-03-18 13:42:33 +0000

    Update NotesRoutes.js

--- backend/Routes/NotesRoutes.js
+++ backend/Routes/NotesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const notesController = require('../controllers/notesController');
 
 router.post('/notes',notesController.createNote);
-router.get('/notes/:id', notesController.getNotes);
+router.get('/notes', notesController.getNotes);
 router.get('/notes/:id', notesController.getNoteById);
 router.put('/notes/:id', notesController.updateNote);
 router.delete('/notes/:id', notesController.deleteNote);
