a9e98f07708310adf3a7065f4608d3ea70b216a9
imaneouarid <imaneomg@gmail.com>
2024-03-18 12:54:55 +0000

    add crud

--- backend/Controllers/NotesControllers.js
+++ backend/Controllers/NotesControllers.js
@@ -4,26 +4,27 @@ const router = express.Router();
 import Note from './NotesModel.js'; 
 
 // Create a new note
-router.post('/notes', async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const newNote = new Note({ title, content });
-    const savedNote = await newNote.save();
-    res.status(201).json(savedNote);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+export const createNote = async (req, res) => {
+    try {
+      const { title, content } = req.body;
+      const newNote = new Note({ title, content });
+      const savedNote = await newNote.save();
+      res.status(201).json(savedNote);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  
 
-$router.get('/notes', async (req, res) => {
-  try {
-    const notes = await Note.find();
-    res.json(notes);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
+  export const getNotes = async (req, res) => {
+    try {
+      const notes = await Note.find();
+      res.json(notes);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
 router.get('/notes/:id', async (req, res) => {
   try {
     const note = await Note.findById(req.params.id);
@@ -36,5 +37,16 @@ router.get('/notes/:id', async (req, res) => {
   }
 });
 
+export const getNoteById = async (req, res) => {
+    try {
+      const note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).json({ message: 'Note not found' });
+      }
+      res.json(note);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
 
-module.exports = router;
+
